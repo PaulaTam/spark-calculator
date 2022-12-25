11e10c82397e9f17b8d7598df6f377dfeecd57bf
PaulaTam <ptam4@sfsu.edu>
2022-12-25 12:24:31 -0800

    Started on GBF calculator

---
+++ src/components/js/GBF.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import { Row, Column } from 'react-bootstrap';
+
+const GBF = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default GBF;
