05a69d5421f73c4af59990046d24f819043cbd5f
PaulaTam <ptam4@sfsu.edu>
2023-01-04 21:45:58 -0800

    set up card grid + css

--- src/components/js/CardGrid.js
+++ src/components/js/CardGrid.js
@@ -1,14 +1,25 @@
+import '../css/CardGrid.css';
 import { Card } from 'react-bootstrap';
 
 const CardGrid = () => {
+
+  const cardInfo = [
+    { link: "/gbf", title: "GBF calculator" }
+  ];
+
+  const renderCard = (card, index) => {
     return (
-        <Card>
+      <Card style={{ width: '18rem' }} key={index} className="box">
         <Card.Body>
-          <Card.Title>GBF Calculator</Card.Title>
-          <Card.Link href='/gbf'>Go to calculator</Card.Link>
+          <Card.Title>{card.title}</Card.Title>
+          <Card.Link href={card.link}>Go to calculator</Card.Link>
         </Card.Body>
       </Card>
     )
-}
+  }
+
+  return (<div className="grid">{cardInfo.map(renderCard)}</div>);
+  
+};
 
 export default CardGrid;
