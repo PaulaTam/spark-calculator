42739358dcb12f6a5069150427d44c3df0ce132f
PaulaTam <ptam4@sfsu.edu>
2023-06-14 11:50:30 -0700

    added initial images to card grid

--- src/components/js/CardGrid.js
+++ src/components/js/CardGrid.js
@@ -4,18 +4,21 @@ import { Card } from 'react-bootstrap';
 const CardGrid = () => {
 
   const cardInfo = [
-    { link: "/gbf", title: "GBF calculator" },
-    { link: "/genshin", title: "Genshin Calculator"},
-    { link: "/prosekai", title: "Project Sekai Calculator"},
+    { link: "/gbf", title: "GBF calculator", img: "../images/wall_of_crystals.png"},
+    { link: "/genshin", title: "Genshin Calculator", img: "../images/genshin-wish.png"},
+    { link: "/prosekai", title: "Project Sekai Calculator", img: "../images/pjsekai-crystals.png"},
   ];
 
   const renderCard = (card, index) => {
     return (
       <Card style={{ width: '18rem' }} key={index} className="box">
+        <Card.Img src={card.img} alt="Card image" style={{opacity: .75}} />
+        <Card.ImgOverlay>
         <Card.Body>
           <Card.Title>{card.title}</Card.Title>
           <Card.Link href={card.link}>Click Here</Card.Link>
         </Card.Body>
+        </Card.ImgOverlay>
       </Card>
     )
   }
