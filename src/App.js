fadec5d78d2a1cf028be88eae0f4c8d2948546de
PaulaTam <ptam4@sfsu.edu>
2023-06-20 15:56:58 -0700

    added about page component, minor edit to css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import CardGrid  from './components/js/CardGrid';
-import TopNavBar from './components/js/TopNavBar';
+import { TopNavBar } from './components/js/TopNavBar';
+import About from './components/js/About';
 
+//all calculator imports here
 import GBF from './components/js/GBF';
 import Genshin from './components/js/Genshin';
 import ProSekai from './components/js/ProSekai';
@@ -12,16 +14,17 @@ function App() {
   return (
     <div className="App">
       <TopNavBar />
-      <Container>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<CardGrid />} />
-          <Route exact path="/gbf" element={<GBF />} />
-          <Route exact path="/genshin" element={<Genshin />} />
-          <Route exact path="/prosekai" element={<ProSekai />} />
-        </Routes>
-      </Router>
-      </Container>
+        <Container>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<CardGrid />} />
+            <Route path="/about" element={<About />} />
+            <Route exact path="/gbf" element={<GBF />} />
+            <Route exact path="/genshin" element={<Genshin />} />
+            <Route exact path="/prosekai" element={<ProSekai />} />
+          </Routes>
+        </Router>
+        </Container>
     </div>
   );
 }
