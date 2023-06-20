4dd1696d79a8bbcaeb40ce247fbc37a88a5a26e1
PaulaTam <ptam4@sfsu.edu>
2023-06-20 16:01:20 -0700

    made index.js for images for importing

---
+++ src/images/index.js
@@ -0,0 +1,16 @@
+import gbf_img from "./thumbnails/gbf_thumb.png";
+import genshin_img from "./thumbnails/genshin_thumb.png";
+import pjsekai_img from "./thumbnails/pjsekai_thumb.png";
+
+//gbf images
+import fate_ep_ex from "./gbf_images/fate_ep_example.png";
+
+export const thumbnails = {
+    gbf_img,
+    genshin_img,
+    pjsekai_img,
+};
+
+export const gbfImgs = {
+    fate_ep_ex,
+};
